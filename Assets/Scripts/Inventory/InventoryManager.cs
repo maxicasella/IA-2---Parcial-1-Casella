4e7bc84a4e4f4e345fb68fdb8e1f37c09b948f7c
maxicasella <maxicasella@gmail.com>
2023-10-16 11:41:39 -0300

    Filtro inventario

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -3,33 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
+public enum FilterType
+{
+    All,
+    Tools,
+    Miscellaneous,
+    Consumibles
+}
 public class InventoryManager : MonoBehaviour
 {
     public List<Slots> items = new List<Slots>();
     [SerializeField] GameObject _slotsHolder;
     GameObject[] _slots;
 
+    [SerializeField] Button _toolsButton;
+    [SerializeField] Button _miscellaneousButton;
+    [SerializeField] Button _consumiblesButton;
+    [SerializeField] Button _inventaryButton;
+
     private void Start()
-    {
+     {
+        _inventaryButton.onClick.AddListener(() => RefreshUI(FilterType.All));
+        _toolsButton.onClick.AddListener(() => RefreshUI(FilterType.Tools));
+        _miscellaneousButton.onClick.AddListener(() => RefreshUI(FilterType.Miscellaneous));
+        _consumiblesButton.onClick.AddListener(() => RefreshUI(FilterType.Consumibles));
+
         _slots = new GameObject[_slotsHolder.transform.childCount];
         for (int i = 0; i < _slotsHolder.transform.childCount; i++)
         {
             _slots[i] = _slotsHolder.transform.GetChild(i).gameObject;
         }
         RefreshUI();
-    }
+     }
 
-    public void RefreshUI()
+    public void RefreshUI(FilterType filterType = FilterType.All)
     {
+        List<Slots> filteredItems;
+
+        if (filterType == FilterType.All) filteredItems = items.ToList(); //IA 2 - Parcial 1
+        else
+        {
+            //IA 2 - Parcial 1
+            filteredItems = items.Where(slot =>
+            {
+                Items currentItem = slot.GetItem();
+                switch (filterType)
+                {
+                    case FilterType.Tools:
+                        return currentItem is Tools;
+                    case FilterType.Miscellaneous:
+                        return currentItem is Miscellaneous;
+                    case FilterType.Consumibles:
+                        return currentItem is Consumibles;
+                    default:
+                        return false;
+                }
+            }).ToList(); //IA 2 - Parcial 1
+        }
+
         for (int i = 0; i < _slots.Length; i++)
         {
-            try 
+            try
             {
-                _slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
-                _slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
-                if(items[i].GetItem().isStackable) _slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = items[i].GetQuantity().ToString();
-                else _slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
+                Slots slot = filteredItems.ElementAtOrDefault(i);
+
+                if (slot != null)
+                {
+                    _slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                    _slots[i].transform.GetChild(0).GetComponent<Image>().sprite = slot.GetItem().itemIcon;
+
+                    if (slot.GetItem().isStackable) _slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = slot.GetQuantity().ToString();
+                    else _slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
+                }
+                else
+                {
+                    _slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                    _slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                    _slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
+                }
             }
             catch
             {
@@ -39,6 +92,7 @@ public class InventoryManager : MonoBehaviour
             }
         }
     }
+
     public bool AddItem(Items item,int value)
     {
         Slots slot = ContainsSlots(item);
@@ -60,30 +114,18 @@ public class InventoryManager : MonoBehaviour
             if(tempSlot.GetQuantity() > 1) tempSlot.Subtract(value);
             else
             {
-                Slots slotRemove = new Slots();
-                foreach (Slots slot in items)
-                {
-                    if (slot.GetItem() == item)
-                    {
-                        slotRemove = slot;
-                        break;
-                    }
-                }
-                items.Remove(slotRemove);
+
+                var slotRemove = items.FirstOrDefault(slot => slot.GetItem() == item); //IA 2 - Parcial 1
+                if (slotRemove != null) items.Remove(slotRemove);
             }
         }
         else return false;
- 
         RefreshUI();
         return true;
     }
 
-    public Slots ContainsSlots(Items item)
+    public Slots ContainsSlots(Items item) //IA 2 - Parcial 1
     {
-        foreach (Slots slot in items)
-        {
-            if (slot.GetItem() == item) return slot;
-        }
-        return null;
+        return items.FirstOrDefault(slot => slot.GetItem()==item);
     }
 }
